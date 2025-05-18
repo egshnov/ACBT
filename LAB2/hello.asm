66eaea0ae741c6ac5eb3cf161d04697d97f1ca9b
egshnov <shalashegdev@gmail.com>
2025-05-18 15:37:02 +0300

    Add actual hello.asm

--- LAB2/hello.asm
+++ LAB2/hello.asm
@@ -3,12 +3,17 @@
 org 100h
 
 start:
-    mov dx, offset message
+    mov dx, offset message1
     mov ah, 9
     int 21h
     
+    mov dx, offset message2
+    mov ah, 9
+    int 21h
+
     mov ax, 4C00h
     int 21h
 
-message db 'Hello from test program!', 13, 10, '$'
+message1 db 'Hello from test program!', 13, 10, '$'
+message1 db 'Second message', 13, 10, '$'
 end start
